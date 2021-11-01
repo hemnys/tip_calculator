862cd6feb5d68e91075704e18932441a432ef23c
Hemnys <hemnys25@gmail.com>
2021-10-31 19:15:52 -0500

    Adding final elements

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const formatNumber = (number) =>
   number !== "" ? parseFloat(number).toFixed(2) : 0;
 const getPercentage = (value) => (value !== "" ? value / 100 : 0);
 const getData = (form) => Array.from(new FormData(form));
-const handleForm = (evt) => {
+const updateTotals = ({ partialTip, totalTip }) => {
   const $tipTotal = document.getElementById("tipTotal");
   const $total = document.getElementById("total");
+  $tipTotal.innerText = formatNumber(partialTip);
+  $total.innerText = formatNumber(totalTip);
+};
+const toggleWarning = (status) => {
   const $warning = document.querySelector(".warning");
+  status
+    ? $warning.classList.remove("hidden")
+    : $warning.classList.add("hidden");
+};
+const cleanForm = () => {
+  const form = document.getElementById("form");
+  const items = [...form.querySelectorAll("input")];
+  items.forEach((item) => {
+    switch (item.type) {
+      case "number":
+        item.value = "0";
+        break;
+      case "radio":
+        item.checked = false;
+        break;
+      default:
+        break;
+    }
+  });
+
+  updateTotals({ partialTip: "0", totalTip: "0" });
+};
+const handleForm = (evt) => {
+  const $reset = document.querySelector(".btn-reset");
   const data = getData(evt.target.closest("form"));
 
   const [bill, tip, customTip, people] = data.map((item) =>
@@ -14,19 +42,19 @@ const handleForm = (evt) => {
   const tipFinalValue = isNaN(tip)
     ? getPercentage(customTip)
     : getPercentage(tip);
-  if ($warning.classList.contains("hidden")) {
-    $warning.classList.remove("hidden");
-  }
+  toggleWarning(true);
   if (people > 0) {
-    $warning.classList.add("hidden");
     let partialTip = (bill * tipFinalValue) / people;
     let totalTip = (bill * (1 + tipFinalValue)) / people;
-    $tipTotal.innerText = formatNumber(partialTip);
-    $total.innerText = formatNumber(totalTip);
+    updateTotals({ partialTip, totalTip });
+    $reset.removeAttribute("disabled");
+    toggleWarning(false);
   }
 };
 window.addEventListener("DOMContentLoaded", function () {
-  const $formWrapper = document.querySelector(".form-wrapper");
-  $formWrapper.addEventListener("change", handleForm);
-  $formWrapper.addEventListener("keyup", handleForm);
+  const $form = document.getElementById("form");
+  const $reset = document.querySelector(".btn-reset");
+  $form.addEventListener("change", handleForm);
+  $form.addEventListener("keyup", handleForm);
+  $reset.addEventListener("click", cleanForm);
 });
